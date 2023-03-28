8fd1161dcc0308b2e913a04e6dc112a1d10136df
mlmcdan1 <77369368+mlmcdan1@users.noreply.github.com>
2023-03-27 20:24:55 -0500

    first commit

---
+++ src/components/Navbar/navbar.js
@@ -0,0 +1,38 @@
+import React, {useState} from "react"
+import './navbar.css'
+
+const Navbar = () => {
+
+    const [burger_class, setBurgerClass] = useState("burger-bar unclicked")
+    const[menu_class, setMenuClass] = useState("menu hidden")
+    const [isMenuClicked, setIsMenuClicked] = useState(false)
+
+    const updateMenu = () => {
+        if(!isMenuClicked){
+            setBurgerClass("burger-bar clicked")
+            setMenuClass("menu visible")
+        }
+        else {
+            setBurgerClass("burger-bar unclicked")
+            setMenuClass("menu hidden")
+        }
+        setIsMenuClicked(!isMenuClicked)
+    }
+    return (
+    
+    <div>
+        <nav>
+            <div className="burger-menu" onClick={updateMenu}>
+                <div className={burger_class}></div>
+                <div className={burger_class}></div>
+                <div className={burger_class}></div>
+            </div>
+        </nav>
+
+        <div className={menu_class}></div>
+      
+    </div>
+  )
+}
+
+export default Navbar
